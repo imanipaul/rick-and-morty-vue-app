adaff53dfd497b05f43f4d5382e19100467d7994
Imani Paul <imani.paul1@gmail.com>
2020-09-21 09:50:06 -0400

    Add styles to browse character page

--- src/components/Browse.vue
+++ src/components/Browse.vue
@@ -1,8 +1,14 @@
 <template>
-  <div>
-    <h1>Browse</h1>
-    <div v-if="!loading">
-      <div v-for="item in data" :key="item.id">{{item.name}}</div>
+  <div class="browse-container">
+    <h1>Browse Characters</h1>
+    <div v-if="!loading" class="character-container">
+      <div v-for="item in data" :key="item.id">
+        <h2>{{item.name}}</h2>
+        <img :src="item.image" />
+        <p>{{item.status}}</p>
+      </div>
+    </div>
+    <div class="button-container">
       <button @click="decreasePage">Previous Page</button>
       <button @click="increasePage">Next Page</button>
     </div>
@@ -31,5 +37,29 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.browse-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 50px 5%;
+  scroll-behavior: smooth;
+
+  .character-container {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+
+  .button-container {
+    width: 50%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-evenly;
+  }
+}
 </style>
