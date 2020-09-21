8c126d0a02f73470f64d0b9c7bb5f8affa4ad59a
Imani Paul <imani.paul1@gmail.com>
2020-09-20 21:21:45 -0400

    Display character name data

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,48 @@
 <template>
   <h1>Rick and Morty Look Up</h1>
-  <Browse />
+  <Browse :data="state.data" :loading="state.loading" />
 </template>
 
 <script>
 import Browse from "./components/Browse.vue";
+import axios from "axios";
+import { ref, onMounted, reactive } from "vue";
 
 export default {
   name: "App",
   components: {
     Browse,
+  },
+  setup() {
+    // const data = ref(null);
+    const loading = ref(true);
+    const error = ref(null);
+
+    const state = reactive({
+      data: "",
+    });
+
+    async function fetchData() {
+      loading.value = true;
+
+      await axios
+        .get("https://rickandmortyapi.com/api/character/")
+        .then((response) => {
+          console.log("response is", response);
+          state.data = response.data.results;
+          loading.value = false;
+        });
+    }
+
+    onMounted(() => {
+      fetchData();
+    });
+
+    return {
+      state,
+      loading,
+      error,
+    };
   },
 };
 </script>
--- src/components/Browse.vue
+++ src/components/Browse.vue
@@ -1,9 +1,24 @@
 <template>
-  <h1>Browse</h1>
+  <div>
+    <h1>Browse</h1>
+    <div v-if="!loading">
+      <div v-for="item in data" :key="item.id">{{item.name}}</div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Browse",
+  props: {
+    data: {
+      type: Object,
+    },
+    loading: {
+      type: Boolean,
+    },
+  },
+};
 </script>
 
 <style>
